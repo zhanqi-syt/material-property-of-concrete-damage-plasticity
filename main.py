e49fc4242af012243f24ee5c815992c433d816ed
YS <suyutai@nwpu.edu.cn>
2022-11-08 15:59:04 +0800

    main_new_flows

--- main.py
+++ main.py
@@ -1,122 +1,84 @@
-from part import *
 from material import *
-from section import *
-from assembly import *
-from step import *
-from interaction import *
-from load import *
-from mesh import *
-from optimization import *
-from job import *
-from sketch import *
-from visualization import *
-from connectorBehavior import *
 import numpy as np
 
-# concrete material parameters
+# Concrete material parameters
 ModelName = 'Model-1'                  # Add your own model name
 MaterName = 'Concr-CDP'                # Add your own material name
-fcr = 50.0                             # Compression strength
-ecr = 0.000001*(700+172.*fcr**0.5)     # Peak strain under compression which is adjustable (empirical relationship)
-e0 = 12285.0*fcr**0.264                # Young's modulus which is adjustable (empirical relationship)
-miu = 0.2                              # Poisson's ratio
-Sigma_y = fcr*0.4                      # Yield stress (0.4~0.6*fcr) which is adjustable (empirical relationship)
-dens = 2.2e-09                         # Density
-delte = 0.0001                         # Strain interval between input points
-maxec = 0.01                           # Max compression strain
-maxet = 0.002                          # Max tension strain
+miu = 0.2                              # Poisson's ratio (from data)
+dens = 2.2e-09                         # Density (from data)
+fcr = 50.0                             # Compression strength (from data)
+e0 = 12285.0*fcr**0.264                # Elastic modulus (empirical relationship)(adjustable)
+ecr = 0.000001*(700+172.*fcr**0.5)     # Peak strain under compression (empirical relationship)(adjustable)
+ftr = 0.1*fcr                          # Representative tension strength (empirical relationship)(adjustable)
+etr = ftr**0.54*0.000065               # Stain for representative tension strength (empirical relationship)(adjustable)
+Sigma_y = fcr*0.4                      # Yield stress (0.4~0.6*fcr)(empirical relationship)(adjustable)
+alpc = 0.157*fcr**0.785-0.905          # Descending params of compression curve (empirical relationship)
+alpt = 0.312*ftr**2                    # Descending params of tension curve (empirical relationship)
+dec = 0.0001                           # Strain increment in compression curve
+dam_c_max = 0.9                        # Maximun compression damage
+det = 0.0001                           # Strain increment in tension curve
+dam_t_max = 0.9                        # Maximun tension damage
 
-# Computing CDP curves
-alpc = 0.157*fcr**0.785-0.905          # Declining parameter of stress-strain curves under compression (empirical relationship)
-ftr = 0.1*fcr                          # Representative tension strength (empirical relationship)
-etr = ftr**0.54*0.000065               # Stain for representative tension strength (empirical relationship)
-alpt = 0.312*ftr**2                    # Declining parameter of stress-strain curves under tension (empirical relationship)
-# Initialization
-Epsilon_c = np.linspace(0.0, maxec, int(maxec/delte)+1)
-Sigma_c = np.zeros(int(maxec/delte)+1)
-Dam_c = np.zeros(int(maxec/delte)+1)
-Epsilon_in_c = np.zeros(int(maxec/delte)+1)
-Epsilon_pl_c = np.zeros(int(maxec/delte)+1)
+# Compression curve
 n = e0 * ecr / (e0 * ecr - fcr)
 rouc = fcr / (e0 * ecr)
-for i in range(int(maxec/delte)+1):
-    if Epsilon_c[i] <= ecr:
-        Sigma_c[i] = Epsilon_c[i]*(rouc*n*e0/(n-1+(Epsilon_c[i]/ecr)**n))
+cpr_all = []
+dam_c = 0.0
+sc = Sigma_y
+ec = Sigma_y/e0
+ec_in = 0
+ec_pl = 0
+i = 0
+while dam_c < dam_c_max:
+    i = i+1
+    # cpr_all: compression information [No., strain, inelastic strain, plastic strain, stress, damage]
+    cpr_all.append([i, ec, ec_in, ec_pl, sc, dam_c])
+    ec = ec+dec
+    if ec <= ecr:
+        sc = ec*(rouc*n*e0/(n-1+(ec/ecr)**n))
     else:
-        Sigma_c[i] = Epsilon_c[i]*(rouc*e0/(alpc*(Epsilon_c[i]/ecr-1)**2+Epsilon_c[i]/ecr))
-    if Epsilon_c[i] != 0:
-        Dam_c[i] = 1.0-np.sqrt(Sigma_c[i]/(Epsilon_c[i]*e0))
-    Epsilon_in_c[i] = Epsilon_c[i]-Sigma_c[i]/e0
-    Epsilon_pl_c[i] = Epsilon_c[i]-Sigma_c[i]/(e0*(1-Dam_c[i]))
+        sc = ec*(rouc*e0/(alpc*(ec/ecr-1)**2+ec/ecr))
+    dam_c = 1.0-np.sqrt(sc/(ec*e0))
+    ec_in = ec - sc/e0
+    ec_pl = ec - sc/(e0*(1-dam_c))
 
 
-Epsilon_t = np.linspace(0.0, maxet, int(maxet/delte)+1)
-Sigma_t = np.zeros(int(maxet/delte)+1)
-Dam_t = np.zeros(int(maxet/delte)+1)
-Epsilon_in_t = np.zeros(int(maxet/delte)+1)
-Epsilon_pl_t = np.zeros(int(maxet/delte)+1)
+# Tension curve
 rout = ftr/(e0*etr)
-for i in range(int(maxet/delte)+1):
-    if Epsilon_t[i] <= etr:
-        Sigma_t[i] = Epsilon_t[i]*e0*(rout*(1.2-0.2*((Epsilon_t[i]/etr)**5)))
-    else:
-        Sigma_t[i] = Epsilon_t[i]*e0*(rout/(alpt*((Epsilon_t[i]/etr-1.0)**1.7)+Epsilon_t[i]/etr))
-    if Epsilon_t[i] != 0:
-        Dam_t[i] = 1.0-np.sqrt(Sigma_t[i]/(Epsilon_t[i]*e0))
-    Epsilon_in_t[i] = Epsilon_t[i]-Sigma_t[i]/e0
-    Epsilon_pl_t[i] = Epsilon_t[i]-Sigma_t[i]/(e0*(1-Dam_t[i]))
+ten_all = []
+dam_t = 0.0
+st = ftr
+et = etr
+et_in = 0
+et_pl = 0
+i = 0
+while dam_t < dam_t_max:
+    i = i+1
+    # ten_all: tension information [No., strain, inelastic strain, plastic strain, stress, damage]
+    ten_all.append([i, et, et_in, et_pl, st, dam_t])
+    et = et + det
+    st = et*e0*(rout/(alpt*((et/etr-1.0)**1.7)+et/etr))
+    dam_t = 1.0-np.sqrt(st/(et*e0))
+    et_in = et-st/e0
+    et_pl = et-st/(e0*(1-dam_t))
 
 
-start_c = np.argmax(Sigma_c > Sigma_y)
-end_c = np.argmax(Dam_c > 0.9)
-start_t = np.argmax(Sigma_t)
-end_t = np.argmax(Dam_t > 0.9)
-if end_c != 0:
-    Harden_c = Epsilon_in_c[start_c:end_c].copy()
-    Damage_c = Dam_c[start_c:end_c].copy()
-    Harden_c[0] = 0.0
-    Damage_c[0] = 0.0
-    Harden_c = np.vstack((Sigma_c[start_c:end_c], Harden_c))
-    Damage_c = np.vstack((Damage_c, Harden_c[1, :]))
-else:
-    Harden_c = Epsilon_in_c[start_c::].copy()
-    Damage_c = Dam_c[start_c::].copy()
-    Harden_c[0] = 0.0
-    Damage_c[0] = 0.0
-    Harden_c = np.vstack((Sigma_c[start_c::], Harden_c))
-    Damage_c = np.vstack((Damage_c, Harden_c[1, :]))
-
-
-if end_t != 0:
-    Stiffen_t = Epsilon_in_t[start_t:end_t].copy()
-    Damage_t = Dam_t[start_t:end_t].copy()
-    Stiffen_t[0] = 0.0
-    Damage_t[0] = 0.0
-    Stiffen_t = np.vstack((Sigma_t[start_t:end_t], Stiffen_t))
-    Damage_t = np.vstack((Damage_t, Stiffen_t[1, :]))
-else:
-    Stiffen_t = Epsilon_in_t[start_t::].copy()
-    Damage_t = Dam_t[start_c::].copy()
-    Stiffen_t[0] = 0.0
-    Damage_t[0] = 0.0
-    Stiffen_t = np.vstack((Sigma_t[start_t::], Stiffen_t))
-    Damage_t = np.vstack((Damage_t, Stiffen_t[1, :]))
-
-
-Harden_c = tuple([tuple(e) for e in Harden_c.T])
-Damage_c = tuple([tuple(e) for e in Damage_c.T])
-Stiffen_t = tuple([tuple(e) for e in Stiffen_t.T])
-Damage_t = tuple([tuple(e) for e in Damage_t.T])
+# Store in tuples
+cpr_all = np.array(cpr_all)
+ten_all = np.array(ten_all)
+Harden_c = tuple([tuple(e) for e in cpr_all[:, [4, 2]]])
+Damage_c = tuple([tuple(e) for e in cpr_all[:, [5, 2]]])
+Stiffen_t = tuple([tuple(e) for e in ten_all[:, [4, 2]]])
+Damage_t = tuple([tuple(e) for e in ten_all[:, [5, 2]]])
 
 # Material Definitions
 m = mdb.models[ModelName]
 m.Material(name=MaterName)
-mat = m.materials['Concre']
+mat = m.materials[MaterName]
 mat.Density(table=((dens, ), ))
 mat.Elastic(table=((e0, miu), ))
-mat.ConcreteDamagedPlasticity(table=((30.0,0.1, 1.16, 0.667, 0.0001), ))
+mat.ConcreteDamagedPlasticity(table=((30.0, 0.1, 1.16, 0.667, 0.0001), ))
 mat.concreteDamagedPlasticity.ConcreteCompressionHardening(table=Harden_c)
 mat.concreteDamagedPlasticity.ConcreteTensionStiffening(table=Stiffen_t)
 mat.concreteDamagedPlasticity.ConcreteCompressionDamage(table=Damage_c)
 mat.concreteDamagedPlasticity.ConcreteTensionDamage(table=Damage_t)
-
